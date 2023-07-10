188f56bc64e0bf1c683461e8bcf86fa484bdc5a4
Guillaume Koch <39165367+gkoch78@users.noreply.github.com>
2023-07-10 14:50:49 -0700

    update

--- tests/unit/hello.py
+++ tests/unit/hello.py
@@ -5,3 +5,6 @@ import council_ai
 class TestHello(unittest.TestCase):
     def test_hello(self):
         self.assertEqual("Hello World!", council_ai.hello_word())
+
+    def test_hello_council(self):
+        self.assertEqual("Hello Council!", council_ai.hello_word("Council"))
