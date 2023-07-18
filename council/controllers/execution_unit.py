fc08f397d9d1c56576473a79e3e6af867d6c3f84
Guillaume Koch <39165367+gkoch78@users.noreply.github.com>
2023-07-18 15:44:57 -0700

    update documentation

--- council/controllers/execution_unit.py
+++ council/controllers/execution_unit.py
@@ -8,6 +8,16 @@ from council.runners import Budget
 
 
 class ExecutionUnit:
+    """
+    Represents an execution unit to be executed by an Agent
+
+    Parameters:
+        chain(Chain): the chain to be executed
+        budget(Budget): the budget granted for this execution
+        initial_state(Optional[ChatMessage]): an optional message that will be injected in the chain context
+        name(Optional[str]): a unique name for the execution. Defaults to :attr:`Chain.name`
+    """
+
     def __init__(
         self, chain: Chain, budget: Budget, initial_state: Optional[ChatMessage] = None, name: Optional[str] = None
     ):
@@ -18,16 +28,40 @@ class ExecutionUnit:
 
     @property
     def chain(self) -> Chain:
+        """
+        The chain to be executed
+
+        Returns:
+            Chain:
+        """
         return self._chain
 
     @property
     def budget(self) -> Budget:
+        """
+        The budget for the execution
+
+        Returns:
+            Budget:
+        """
         return self._budget
 
     @property
     def initial_state(self) -> Optional[ChatMessage]:
+        """
+        An optional message to put in the chain context
+
+        Returns:
+            Optional[ChatMessage]
+        """
         return self._initial_state
 
     @property
     def name(self) -> str:
+        """
+        Name of the execution unit
+
+        Returns:
+            str:
+        """
         return self._name
