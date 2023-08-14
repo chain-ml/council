eacf6e3cf27aa991150568007a05b86da086eac1
Guillaume Koch <39165367+gkoch78@users.noreply.github.com>
2023-08-14 10:22:25 -0700

    Create test_openai_llm_configuration.py

---
+++ tests/unit/llm/test_openai_llm_configuration.py
@@ -0,0 +1,15 @@
+import unittest
+from council.llm import OpenAILLMConfiguration
+from council.utils import OsEnviron
+
+
+class TestOpenAILLMConfiguration(unittest.TestCase):
+    def test_model_default_value(self):
+        with OsEnviron("OPENAI_API_KEY", "akey"):
+            config = OpenAILLMConfiguration.from_env()
+            self.assertEqual("gpt-3.5-turbo", config.model.unwrap())
+
+    def test_model_override(self):
+        with OsEnviron("OPENAI_API_KEY", "akey"), OsEnviron("OPENAI_LLM_MODEL", "amodel"):
+            config = OpenAILLMConfiguration.from_env()
+            self.assertEqual("amodel", config.model.unwrap())
