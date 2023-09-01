f5fcf0d84b6ba7eff996f23a0d0a51540a1fccf3
Guillaume Koch <39165367+gkoch78@users.noreply.github.com>
2023-09-01 15:40:06 -0700

    clean up

--- council/contexts/__init__.py
+++ council/contexts/__init__.py
@@ -3,12 +3,12 @@ contains the core classes of the engine
 """
 
 from ._agent_context import AgentContext
-from ._budget import Consumption, ConsumptionEvent, Budget, InfiniteBudget
+from ._budget import Budget, Consumption, ConsumptionEvent, InfiniteBudget
 from ._cancellation_token import CancellationToken
 from ._chain_context import ChainContext
-from ._chat_message import ChatMessageKind, ChatMessage, ScoredChatMessage
 from ._chat_history import ChatHistory
-from ._execution_log_entry import ExecutionLogEntry
+from ._chat_message import ChatMessage, ChatMessageKind, ScoredChatMessage
 from ._execution_log import ExecutionLog
+from ._execution_log_entry import ExecutionLogEntry
 from ._llm_context import LLMContext
-from ._skill_context import SkillContext, IterationContext
+from ._skill_context import IterationContext, SkillContext
