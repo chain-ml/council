38dc4aebce0016449705ad64cdae90f075ced555
Guillaume Koch <39165367+gkoch78@users.noreply.github.com>
2023-09-11 14:22:31 -0700

    execution log

--- council/contexts/_execution_log.py
+++ council/contexts/_execution_log.py
@@ -5,18 +5,40 @@ from ._execution_log_entry import ExecutionLogEntry
 
 
 class ExecutionLog:
+    """
+    represents the log of execution for each executable items (i.e. :class:`~council.agents.Agent`,
+    :class:`~council.chains.Chain`, :class:`~council.skills.SkillBase` ...)
+    """
+
     def __init__(self):
         self._entries = []
 
     def new_entry(self, name: str) -> ExecutionLogEntry:
+        """
+        adds a new entry into the log
+        Args:
+            name: name of the new entry
+
+        Returns:
+            the newly added entry
+        """
         result = ExecutionLogEntry(name)
         self._entries.append(result)
         return result
 
     def to_json(self) -> str:
+        """
+        serialize the execution log as a `json` string
+
+        Returns:
+            a `json` string
+        """
         return json.dumps(self.to_dict(), indent=2)
 
     def to_dict(self) -> Dict[str, Any]:
+        """
+        convert into a dictionary
+        """
         result = {"entries": [item.to_dict() for item in self._entries]}
 
         return result
