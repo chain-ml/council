99f5d5743d1b50185aeee730d1f128c8ae409bf7
Jérémy d'Auria <jeremydauria@chainml.net>
2023-09-29 18:51:24 +0200

    test(runner): add test with predicate failure

--- tests/unit/runners/test_do_while.py
+++ tests/unit/runners/test_do_while.py
@@ -1,17 +1,17 @@
 from council.contexts import Budget, Consumption, ChainContext
-from council.runners import RunnerSkillError
+from council.runners import RunnerSkillError, RunnerPredicateError
 from council.runners.do_while_runner import DoWhile
 from .helpers import RunnerTestCase, SkillTest, MySkillException
 
 
-class TestWhile(RunnerTestCase):
+class TestDoWhile(RunnerTestCase):
     def test_monitors(self):
         skill = SkillTest("maybe", 0.1)
         instance = DoWhile(lambda _: True, skill)
 
         self.assertEqual(instance.monitor.children["doWhileBody"].name, skill.name)
 
-    def test_while_runner_true(self):
+    def test_do_while_runner_true(self):
         budget_kind = "retry"
         skill = SkillTest("skill", 0.1, budget_kind)
         instance = DoWhile(self.run_while_budget_is_not_expired, skill)
@@ -20,7 +20,7 @@ class TestWhile(RunnerTestCase):
         self.execute(instance, budget)
         self.assertSuccessMessages(["skill" for _ in range(11)])
 
-    def test_while_runner_false(self):
+    def test_do_while_runner_false(self):
         skill = SkillTest("skill", 0.1)
         instance = DoWhile(lambda _: False, skill)
 
@@ -28,14 +28,26 @@ class TestWhile(RunnerTestCase):
         self.execute(instance, budget)
         self.assertSuccessMessages(["skill"])
 
-    def test_while_runner_with_error(self):
-        instance = DoWhile(lambda a: True, SkillTest("maybe", -0.1))
+    def test_do_while_runner_with_error(self):
+        instance = DoWhile(lambda a: True, SkillTest("skill", -0.1))
         with self.assertRaises(RunnerSkillError) as cm:
             self.execute(instance, Budget(1))
 
         self.assertIsInstance(cm.exception.__cause__, MySkillException)
         self.assertSuccessMessages([])
 
+    def test_do_while_runner_with_predicate_exception(self):
+        instance = DoWhile(self.predicate_raising_exception, SkillTest("skill", .1))
+        with self.assertRaises(RunnerPredicateError) as cm:
+            self.execute(instance, Budget(1))
+
+        self.assertSuccessMessages(['skill'])
+
     @staticmethod
     def run_while_budget_is_not_expired(context: ChainContext) -> bool:
         return not context.budget.is_expired()
+
+    @staticmethod
+    def predicate_raising_exception(context: ChainContext) -> bool:
+        raise "the predicate function raised an exception"
+
