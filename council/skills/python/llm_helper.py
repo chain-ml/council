5abd3704416372a5c208d1ce9ce2c463356d58d4
Guillaume Koch <39165367+gkoch78@users.noreply.github.com>
2023-10-25 16:17:20 -0700

    Update llm_helper.py

--- council/skills/python/llm_helper.py
+++ council/skills/python/llm_helper.py
@@ -1,21 +1,15 @@
+from council.utils import CodeParser
+
+
 class LLMMessageParseException(Exception):
     pass
 
 
 def extract_code_block(text: str, block_type: str = "") -> str:
-    start_token = f"```{block_type}\n"
-    end_token = "\n```"
-    start = text.find(start_token)
-    if start == -1:
+    result = CodeParser.find_first(block_type, text)
+    if result is None:
         if block_type == "":
             raise LLMMessageParseException("could not find a code block")
         else:
             raise LLMMessageParseException(f"could not find a code block of type `{block_type}`")
-    start += len(start_token)
-    end = text.find(end_token, start)
-    if end == -1:
-        if block_type == "":
-            raise LLMMessageParseException("could not find the end of the code block")
-        else:
-            raise LLMMessageParseException(f"could not find the end of the code block of type `{block_type}`")
-    return text[start:end]
+    return result.code
