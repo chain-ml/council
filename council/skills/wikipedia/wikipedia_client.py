77cdd33ab01e69ff50a8c1e41c155f52b0cb856e
Guillaume Koch <39165367+gkoch78@users.noreply.github.com>
2023-11-02 13:35:11 -0700

    add WikipediaSearchSkill

---
+++ council/skills/wikipedia/wikipedia_client.py
@@ -0,0 +1,76 @@
+from typing import Dict, List, Optional
+
+import bs4
+from mediawiki import MediaWiki, MediaWikiPage
+
+
+class WikipediaPageSection:
+    """
+    Represents partial content from a Wikipedia Page
+    """
+
+    def __init__(self, title: str, content: str, page_id: int):
+        self._title = title
+        self._content = content
+        self._page_id = page_id
+
+    @property
+    def title(self) -> str:
+        """
+        Returns:
+            str: the page title
+
+        """
+        return self._title
+
+    @property
+    def content(self) -> str:
+        return self._content
+
+    @property
+    def page_id(self) -> int:
+        return self._page_id
+
+    def to_dict(self) -> Dict[str, str]:
+        return {"title": self._title, "content": self._content, "page_id": self._page_id}
+
+
+class WikipediaClient:
+    def __init__(self, url: str = "https://en.wikipedia.org/w/api.php"):
+        self.wikipedia = MediaWiki(url=url)
+
+    def search_pages_custom(self, text: str, count: int) -> List[WikipediaPageSection]:
+        result = []
+        search_result = self.wikipedia.wiki_request(
+            {
+                "action": "query",
+                "list": "search",
+                "srsearch": text,
+            }
+        )
+
+        for item in search_result["query"]["search"]:
+            title = item["title"]
+            content = self.clean_text(item["snippet"])
+            page_id = item["pageid"]
+            result.append(WikipediaPageSection(title=title, content=content, page_id=page_id))
+            if len(result) >= count:
+                break
+        return result
+
+    def search_page_section(self, page_id: int, text: str) -> Optional[WikipediaPageSection]:
+        normalized_text = text.lower()
+        page = self.get_page_by_id(page_id)
+        if page is None:
+            return None
+        for section in [None] + page.sections:
+            content = page.section(section)
+            if normalized_text in content.lower():
+                return WikipediaPageSection(title=section, content=content, page_id=page_id)
+
+    def get_page_by_id(self, page_id: int) -> Optional[MediaWikiPage]:
+        return self.wikipedia.page(pageid=page_id)
+
+    @staticmethod
+    def clean_text(text: str) -> str:
+        return bs4.BeautifulSoup(text, "html.parser").text
---
+++ tests/unit/skills/wikipedia/test_wikipedia_client.py
@@ -0,0 +1,30 @@
+import unittest
+
+from council.skills.wikipedia import WikipediaClient
+
+
+class TestWikipediaSkill(unittest.TestCase):
+    def test_search_page_custom(self):
+        instance = WikipediaClient()
+        result = instance.search_pages_custom("Python Programming Language", 3)
+
+        assert len(result) == 3
+        assert result[0].page_id == 23862
+        assert result[0].title == "Python (programming language)"
+        assert "programming language" in result[0].content
+
+    def test_search_page_content(self):
+        instance = WikipediaClient()
+        result = instance.search_page_section(page_id=23862, text="whitespace")
+
+        assert result.title == "Indentation"
+        assert result.content.startswith("Python uses whitespace indentation,")
+
+    def test_clean_text(self):
+        text = (
+            '<span class="searchmatch">Python</span> is a high-level, general-purpose'
+            ' <span class="searchmatch">programming</span> <span class="searchmatch">language</span>.'
+            " Its design philosophy emphasizes code readability with the use of significant indentation"
+        )
+        result = WikipediaClient.clean_text(text)
+        assert "programming language" in result
