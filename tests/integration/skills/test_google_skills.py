af5faa7f9fab207d65c92a3359508b3853afe6eb
Jerry Zikun Chen <zikunchen@outlook.com>
2023-12-14 09:20:44 -0500

    Add assertions for json fields

--- tests/integration/skills/test_google_skills.py
+++ tests/integration/skills/test_google_skills.py
@@ -2,6 +2,8 @@ import unittest
 
 import dotenv
 
+import json
+
 from council.contexts import ChainContext, Budget, ChatHistory, SkillContext
 from council.skills.google import GoogleNewsSkill, GoogleSearchSkill
 from council.skills.google.google_context import GoogleNewsSearchEngine, GoogleSearchEngine
@@ -32,6 +34,11 @@ class TestBase(unittest.TestCase):
         result = skill.execute(SkillContext.from_chain_context(context, Option.none()))
         self.assertTrue(result.is_ok)
         self.assertIn("EUR", result.message)
+        for d in json.loads(result.data):
+            self.assertGreater(len(d["title"]), 0)
+            self.assertGreater(len(d["url"]), 0)
+            self.assertEqual(len(d["snippet"]), 0)
+            self.assertIsNotNone(d["date"])
 
     def test_gsearch_skill(self):
         context = ChainContext.from_user_message("USD", budget=Budget(duration=10))
@@ -40,6 +47,12 @@ class TestBase(unittest.TestCase):
         result = skill.execute(SkillContext.from_chain_context(context, Option.none()))
         self.assertTrue(result.is_ok)
         self.assertIn("USD", result.message)
+        print(result.data)
+        for d in json.loads(result.data):
+            self.assertGreater(len(d["title"]), 0)
+            self.assertGreater(len(d["url"]), 0)
+            self.assertGreater(len(d["snippet"]), 0)
+            self.assertIsNone(d["date"])
 
     def test_skill_no_message(self):
         context = ChainContext.from_chat_history(ChatHistory(), Budget(duration=10))
