04287f0c469eeb26432a59489c38e9281c4ecd9f
Arnaud Flament <arnaud.flament@gmail.com>
2024-01-29 14:04:36 -0800

    test for new parameters

--- tests/integration/skills/test_google_skills.py
+++ tests/integration/skills/test_google_skills.py
@@ -1,4 +1,5 @@
 import unittest
+from datetime import datetime, timedelta
 
 import dotenv
 
@@ -14,13 +15,13 @@ class TestBase(unittest.TestCase):
     def setUp(self) -> None:
         dotenv.load_dotenv()
 
-    def test_gnews(self):
+    def test_gnews_engine(self):
         expected = 8
         gn = GoogleNewsSearchEngine(period="90d", suffix="Finance")
         resp = gn.execute(query="USD", nb_results=expected)
         self.assertEqual(len(resp), expected)
 
-    def test_gsearch(self):
+    def test_gsearch_engine(self):
         expected = 8
         gn = GoogleSearchEngine.from_env()
         resp = gn.execute(query="USD", nb_results=expected)
@@ -30,24 +31,32 @@ class TestBase(unittest.TestCase):
         context = ChainContext.from_user_message("USD", Budget(duration=10))
         context.chat_history.add_user_message("EUR")
 
-        skill = GoogleNewsSkill(suffix="Finance")
+        expected_result_count = 4
+        skill = GoogleNewsSkill(suffix="Finance", nb_results=expected_result_count, period="15d")
         result = skill.execute(SkillContext.from_chain_context(context, Option.none()))
         self.assertTrue(result.is_ok)
-        self.assertIn("EUR", result.message)
-        for d in json.loads(result.data):
+
+        json_loads = json.loads(result.data)
+        self.assertLessEqual(expected_result_count, len(json_loads))
+        for d in json_loads:
             self.assertGreater(len(d["title"]), 0)
             self.assertGreater(len(d["url"]), 0)
             self.assertEqual(len(d["snippet"]), 0)
-            self.assertIsNotNone(d["date"])
+            self.assertTrue(is_within_period(d["date"], 15))
 
     def test_gsearch_skill(self):
         context = ChainContext.from_user_message("USD", budget=Budget(duration=10))
 
-        skill = GoogleSearchSkill()
+        expected_result_count = 7
+        skill = GoogleSearchSkill(nb_results=expected_result_count)
         result = skill.execute(SkillContext.from_chain_context(context, Option.none()))
+
         self.assertTrue(result.is_ok)
         self.assertIn("USD", result.message)
-        for d in json.loads(result.data):
+
+        json_loads = json.loads(result.data)
+        self.assertEqual(expected_result_count, len(json_loads))
+        for d in json_loads:
             self.assertGreater(len(d["title"]), 0)
             self.assertGreater(len(d["url"]), 0)
             self.assertGreater(len(d["snippet"]), 0)
@@ -61,3 +70,30 @@ class TestBase(unittest.TestCase):
             self.assertTrue(False)
         except OptionException as oe:
             self.assertTrue(oe)
+
+
+def parse_relative_timestamp(timestamp: str) -> datetime:
+    if "minute" in timestamp:
+        minutes_ago = int(timestamp.split()[0])
+        return datetime.now() - timedelta(minutes=minutes_ago)
+    if "hour" in timestamp:
+        hours_ago = int(timestamp.split()[0])
+        return datetime.now() - timedelta(hours=hours_ago)
+    if "day" in timestamp:
+        days_ago = int(timestamp.split()[0])
+        return datetime.now() - timedelta(days=days_ago)
+    if "week" in timestamp:
+        weeks_ago = int(timestamp.split()[0])
+        return datetime.now() - timedelta(weeks=weeks_ago)
+    else:
+        raise ValueError("Unsupported relative timestamp format")
+
+
+def is_within_period(result: str, period: int) -> bool:
+    if "ago" in result:
+        publication_date = parse_relative_timestamp(result)
+    else:
+        publication_date = datetime.strptime(result, "%Y-%m-%d")
+
+    delta = datetime.now() - publication_date
+    return delta <= timedelta(days=period)
