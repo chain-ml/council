c2aade3d5eff75ff58e724bc1986db5a3cf316bc
PXLKSR <te@nexttuesday.de>
2024-02-25 13:44:51 +0100

    support custom API host and improve docstrings
    - Add api_host parameter to support custom OpenAI API host
    - Update docstrings for clarity and documentation purposes
    - Adjust imports to multiple lines for readability

--- council/llm/openai_llm_configuration.py
+++ council/llm/openai_llm_configuration.py
@@ -3,7 +3,13 @@ from typing import Any, Optional
 
 from council.llm import LLMConfigurationBase
 from council.llm.llm_config_object import LLMConfigSpec
-from council.utils import read_env_str, read_env_int, Parameter, greater_than_validator, prefix_validator
+from council.utils import (
+    read_env_str,
+    read_env_int,
+    Parameter,
+    greater_than_validator,
+    prefix_validator,
+)
 from council.llm.llm_configuration_base import _DEFAULT_TIMEOUT
 
 _env_var_prefix = "OPENAI_"
@@ -17,11 +23,12 @@ class OpenAILLMConfiguration(LLMConfigurationBase):
         * see https://platform.openai.com/docs/api-reference/chat
     """
 
-    def __init__(self, api_key: str, model: str, timeout: int = _DEFAULT_TIMEOUT):
+    def __init__(self, api_key: str, api_host: str, model: str, timeout: int = _DEFAULT_TIMEOUT):
         """
         Initialize a new instance of OpenAILLMConfiguration
         Args:
             api_key (str): the OpenAI api key
+            api_host (str): the OpenAI Host
             model (str): model version to use
             timeout (int): seconds to wait for response from OpenAI before timing out
         """
@@ -32,6 +39,13 @@ class OpenAILLMConfiguration(LLMConfigurationBase):
         )
         self._api_key = Parameter.string(
             name="api_key", required=True, value=api_key, validator=prefix_validator("sk-")
+        )
+        self._api_host = Parameter.string(
+            name="api_host",
+            required=False,
+            value=api_host,
+            default="https://api.openai.com",
+            validator=None,
         )
 
     @property
@@ -49,6 +63,13 @@ class OpenAILLMConfiguration(LLMConfigurationBase):
         return self._api_key
 
     @property
+    def api_host(self) -> Parameter[str]:
+        """
+        OpenAI API Host
+        """
+        return self._api_host
+
+    @property
     def timeout(self) -> Parameter[int]:
         """
         API timeout
@@ -62,22 +83,29 @@ class OpenAILLMConfiguration(LLMConfigurationBase):
         return payload
 
     @staticmethod
-    def from_env(model: Optional[str] = None) -> OpenAILLMConfiguration:
+    def from_env(model: Optional[str] = None, api_host: Optional[str] = None) -> OpenAILLMConfiguration:
         api_key = read_env_str(_env_var_prefix + "API_KEY").unwrap()
+        if api_host is None:
+            api_host = read_env_str(
+                _env_var_prefix + "API_HOST", required=False, default="https://api.openai.com"
+            ).unwrap()
+
         if model is None:
             model = read_env_str(_env_var_prefix + "LLM_MODEL", required=False, default="gpt-3.5-turbo").unwrap()
+
         timeout = read_env_int(_env_var_prefix + "LLM_TIMEOUT", required=False, default=_DEFAULT_TIMEOUT).unwrap()
 
-        config = OpenAILLMConfiguration(model=model, api_key=api_key, timeout=timeout)
+        config = OpenAILLMConfiguration(model=model, api_key=api_key, api_host=api_host, timeout=timeout)
         config.read_env(_env_var_prefix)
         return config
 
     @staticmethod
     def from_spec(spec: LLMConfigSpec) -> OpenAILLMConfiguration:
         api_key: str = spec.provider.must_get_value("apiKey")
+        api_host: Any = spec.provider.get_value("apiHost")
         model: str = spec.provider.must_get_value("model")
 
-        config = OpenAILLMConfiguration(api_key=api_key, model=str(model))
+        config = OpenAILLMConfiguration(api_key=api_key, api_host=api_host, model=str(model))
         if spec.parameters is not None:
             config.from_dict(spec.parameters)
 
