0c020c2102c56c429f35e0515e2194d1fe81e82b
Arnaud Flament <arnaud.flament@gmail.com>
2024-05-29 17:50:54 -0700

    Update agent_chain.py

--- council/agents/agent_chain.py
+++ council/agents/agent_chain.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional
+from typing import Optional
 
 from council.chains import ChainBase
 from council.contexts import AgentContext, ChainContext, ChatMessage, Monitored
