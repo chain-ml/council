004020fa3e75ba50f953a44ca8126a5eeda6bbf6
Dmytro Nikolaiev <nikolaevdmitryprog@gmail.com>
2024-10-22 15:40:27 -0400

    Use EchoResponseParser and assert on wall time

--- tests/integration/llm/test_llm_cache_middleware.py
+++ tests/integration/llm/test_llm_cache_middleware.py
@@ -6,6 +6,7 @@ import dotenv
 from council import AnthropicLLM
 from council.llm.llm_function import LLMFunction
 from council.llm.llm_middleware import LLMCachingMiddleware, LLMResponse
+from council.llm.llm_response_parser import EchoResponseParser
 from council.utils import OsEnviron
 
 
@@ -16,12 +17,12 @@ class TestLlmCachingMiddleware(unittest.TestCase):
             self.llm = AnthropicLLM.from_env()
 
         self.llm_func: LLMFunction[LLMResponse] = LLMFunction(
-            self.llm, lambda response: response, system_message="You're a helpful assistant"
+            self.llm, EchoResponseParser.from_response, system_message="You're a helpful assistant"
         )
 
         self.llm_func.add_middleware(LLMCachingMiddleware(ttl=60))
 
-    def execute_llm_func(self, message: str, to_print: str, **kwargs) -> None:
+    def get_llm_func_wall_time(self, message: str, to_print: str, **kwargs) -> float:
         start_time = time.time()
         first_response = self.llm_func.execute(message, **kwargs)
         wall_time = time.time() - start_time
@@ -29,12 +30,18 @@ class TestLlmCachingMiddleware(unittest.TestCase):
         print(f"\tResponse duration: {first_response.duration:.3f}s")
         print(f"\tWall time: {wall_time:.3f}s")
 
+        return wall_time
+
     def test_caching(self):
-        # assert on wall_time
-        self.execute_llm_func("What is the capital of France?", to_print="First request")
-        self.execute_llm_func("What is the capital of France?", to_print="Second request (cached)")
-        self.execute_llm_func("What is the capital of France???", to_print="Third request (different input)")
-        self.execute_llm_func(
-            "What is the capital of France?", to_print="Fourth request (different params)", temperature=0.9
-        )
-        self.execute_llm_func("What is the capital of France?", to_print="Fifth request (cached)", temperature=0.9)
+        message_v1 = "What is the capital of France?"
+        message_v2 = "What is the capital of France???"
+        kwargs = {"temperature": 0.9}
+
+        _ = self.get_llm_func_wall_time(message_v1, to_print="First request")
+        t = self.get_llm_func_wall_time(message_v1, to_print="Second request (cached)")
+        assert t < 0.1
+
+        _ = self.get_llm_func_wall_time(message_v2, to_print="Third request (different input)")
+        _ = self.get_llm_func_wall_time(message_v1, to_print="Fourth request (different params)", **kwargs)
+        t = self.get_llm_func_wall_time(message_v1, to_print="Fifth request (cached)", **kwargs)
+        assert t < 0.1
