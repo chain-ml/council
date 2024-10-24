c9912cbb3f473fc376d916c910a7481c385c7e8e
Dmytro Nikolaiev <nikolaevdmitryprog@gmail.com>
2024-10-24 15:17:37 -0400

    Address comments

--- tests/integration/llm/test_llm_caching_middleware.py
+++ tests/integration/llm/test_llm_caching_middleware.py
@@ -36,11 +36,11 @@ class TestLlmCachingMiddleware(unittest.TestCase):
         return response
 
     @staticmethod
-    def assert_if_cached(response):
+    def raise_if_cached(response):
         assert all(not consumption.unit.startswith("cached_") for consumption in response.result.consumptions)
 
     @staticmethod
-    def assert_if_not_cached(response):
+    def raise_if_not_cached(response):
         assert response.duration == 0
         assert all(consumption.unit.startswith("cached_") for consumption in response.result.consumptions)
 
@@ -49,60 +49,60 @@ class TestLlmCachingMiddleware(unittest.TestCase):
         kwargs = {"temperature": 0.9}
 
         response = self.execute_llm_func(llm_func, self.m1, to_print="message_v1")
-        self.assert_if_cached(response)
+        self.raise_if_cached(response)
 
         response = self.execute_llm_func(llm_func, self.m1, to_print="message_v1 (cached)")
-        self.assert_if_not_cached(response)
+        self.raise_if_not_cached(response)
 
         response = self.execute_llm_func(llm_func, self.m2, to_print="message_v2")
-        self.assert_if_cached(response)
+        self.raise_if_cached(response)
 
         response = self.execute_llm_func(llm_func, self.m1, to_print="message_v1 + kwargs", **kwargs)
-        self.assert_if_cached(response)
+        self.raise_if_cached(response)
 
         response = self.execute_llm_func(llm_func, self.m1, to_print="message_v1 + kwargs (cached)", **kwargs)
-        self.assert_if_not_cached(response)
+        self.raise_if_not_cached(response)
 
     def test_ttl(self):
         llm_func = self.get_llm_func(ttl=2, cache_limit_size=10)
 
         response = self.execute_llm_func(llm_func, self.m1, to_print="message_v1")
-        self.assert_if_cached(response)
+        self.raise_if_cached(response)
 
         response = self.execute_llm_func(llm_func, self.m1, to_print="message_v1 (cached)")
-        self.assert_if_not_cached(response)
+        self.raise_if_not_cached(response)
 
-        time.sleep(2)
+        time.sleep(2.5)
         response = self.execute_llm_func(llm_func, self.m1, to_print="message_v1 after 2s sleep (should be expired)")
-        self.assert_if_cached(response)
+        self.raise_if_cached(response)
 
     def test_cache_limit_size(self):
         llm_func = self.get_llm_func(ttl=60, cache_limit_size=2)
 
         response = self.execute_llm_func(llm_func, self.m1, to_print="message_v1")
         # cache: m1 <- latest
-        self.assert_if_cached(response)
+        self.raise_if_cached(response)
 
         response = self.execute_llm_func(llm_func, self.m2, to_print="message_v2")
         # cache: m1, m2
-        self.assert_if_cached(response)
+        self.raise_if_cached(response)
 
         response = self.execute_llm_func(llm_func, self.m3, to_print="message_v3")
         # cache: m2, m3
-        self.assert_if_cached(response)
+        self.raise_if_cached(response)
 
         response = self.execute_llm_func(llm_func, self.m3, to_print="message_v3 (cached)")
         # cache: m2, m3
-        self.assert_if_not_cached(response)
+        self.raise_if_not_cached(response)
 
         response = self.execute_llm_func(llm_func, self.m2, to_print="message_v2 (cached)")
         # cache: m3, m2
-        self.assert_if_not_cached(response)
+        self.raise_if_not_cached(response)
 
         response = self.execute_llm_func(llm_func, self.m1, to_print="message_v1 (not cached due to size limits)")
         # cache: m2, m1
-        self.assert_if_cached(response)
+        self.raise_if_cached(response)
 
         response = self.execute_llm_func(llm_func, self.m3, to_print="message_v3 (not cached due to size limits)")
         # cache: m1, m3
-        self.assert_if_cached(response)
+        self.raise_if_cached(response)
