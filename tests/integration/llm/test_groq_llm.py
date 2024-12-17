525e9bd3922d15ce449493d2151ee25684416e2b
Nikolaiev Dmytro <nikolaevdmitryprog@gmail.com>
2024-12-17 14:37:05 -0500

    Feature update Groq costs (#201)
    * Update costs file
    
    * Update model used in test
    
    * Delete alias
    
    * Update unit tests
    
    * Delete gemma-7b-it from costs (deprecated 2024-12-18)

--- tests/integration/llm/test_groq_llm.py
+++ tests/integration/llm/test_groq_llm.py
@@ -10,14 +10,14 @@ from council.utils import OsEnviron
 
 class TestGroqLLM(unittest.TestCase):
     @staticmethod
-    def get_gemma_7b():
+    def get_gemma():
         dotenv.load_dotenv()
-        with OsEnviron("GROQ_LLM_MODEL", "gemma-7b-it"):
+        with OsEnviron("GROQ_LLM_MODEL", "gemma2-9b-it"):
             return GroqLLM.from_env()
 
     def test_message(self):
         messages = [LLMMessage.user_message("what is the capital of France?")]
-        llm = self.get_gemma_7b()
+        llm = self.get_gemma()
 
         result = llm.post_chat_request(LLMContext.empty(), messages)
         assert "Paris" in result.choices[0]
@@ -29,15 +29,15 @@ class TestGroqLLM(unittest.TestCase):
 
     def test_consumptions(self):
         messages = [LLMMessage.user_message("Hello how are you?")]
-        llm = self.get_gemma_7b()
+        llm = self.get_gemma()
         result = llm.post_chat_request(LLMContext.empty(), messages)
 
         assert len(result.consumptions) == 12  # call, duration, 3 token kinds, 3 cost kinds and 4 groq duration
         for consumption in result.consumptions:
-            assert consumption.kind.startswith("gemma-7b-it")
+            assert consumption.kind.startswith("gemma2-9b-it")
 
     def test_max_tokens_param(self):
-        llm = self.get_gemma_7b()
+        llm = self.get_gemma()
         llm.configuration.temperature.set(0.8)
         llm.configuration.max_tokens.set(7)
 
@@ -47,7 +47,7 @@ class TestGroqLLM(unittest.TestCase):
 
     def test_json_mode(self):
         messages = [LLMMessage.user_message("Output a JSON object with the data about RPG character.")]
-        llm = self.get_gemma_7b()
+        llm = self.get_gemma()
         result = llm.post_chat_request(LLMContext.empty(), messages, response_format={"type": "json_object"})
 
         data = json.loads(result.first_choice)
